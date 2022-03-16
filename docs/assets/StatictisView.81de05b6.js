69b28718444eaf48068e95bd65d8e2defdeaa5f3
devem-software <devem.software@gmail.com>
2022-03-16 16:25:50 -0500

    UPDATE: Router base path

---
+++ docs/assets/StatictisView.81de05b6.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.a17eec9e.js";import{o as t,c as s,a}from"./vendor.6d20fed3.js";const _={},o={class:"about"},c=a("h1",null,"This is an about page",-1),i=[c];function n(r,d){return t(),s("div",o,i)}var f=e(_,[["render",n]]);export{f as default};

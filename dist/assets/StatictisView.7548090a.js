68c37f48d8713de25a17ddc3aeb5802bf52233f6
devem-software <devem.software@gmail.com>
2022-03-16 14:52:26 -0500

    FEATURE: First commit to website FEALURE: Implement ssl certificate

---
+++ dist/assets/StatictisView.7548090a.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.65c43b4b.js";import{o as t,c as s,a}from"./vendor.37060039.js";const _={},o={class:"about"},c=a("h1",null,"This is an about page",-1),i=[c];function n(r,d){return t(),s("div",o,i)}var f=e(_,[["render",n]]);export{f as default};
